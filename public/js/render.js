b2d0d5f88bfe500053d5d17b19b29babe049cf5b
Jeffrey Ying <jeffrey.ying86@live.com>
2015-02-17 15:03:50 -0500

    Updated layout

--- public/js/render.js
+++ public/js/render.js
@@ -1,20 +1,39 @@
-var main = function() {
-  $('.mathinput').keyup(function() {
-    var text = $('.mathinput').val();
-    var done = function (){
-      $('.mathguy').show();
-    }
-    $('.mathguy').hide();
-    if (text==""){
-      $('.mathguy').text("");
-    } else {
-      $('.mathguy').text("$"+text+"$");
-      MathJax.Hub.Queue(
-        ["Typeset",MathJax.Hub],
-        ["",done]);
-    }
-    
 
-  });
-}
-$(document).ready(main);  
+var render = {
+
+  init: function() {
+    render.updateDisplay();
+    render.genImage();
+  },
+
+  //When a user types something, execute this
+  updateDisplay: function (){
+    $('.mathinput').keyup(function() {
+      var text = $('.mathinput').val();
+      var done = function (){
+        $('.mathguy').show();
+      }
+      $('.mathguy').hide();
+      if (text==""){
+        $('.mathguy').text("");
+      } else {
+        $('.mathguy').text("$"+text+"$");
+        MathJax.Hub.Queue(
+          ["Typeset",MathJax.Hub],
+          ["",done]);
+      }
+    });
+  },
+
+  //When the gen-img button is pressed
+  genImage: function() {
+    $('.gen-img').click(function (){
+      var mathtxt = $('.mathinput').val();
+      var text = '<a href="http://latex.codecogs.com/gif.latex?' + mathtxt + '">Link to Img</a>';
+      $('.img-link').html(text);
+    });
+  }
+
+
+};
+$(document).ready(render.init);  
